8707110e9b79792e679a2eb3679937d4b02e2129
Sergei Shikalov <ssh@42px.org>
2020-06-23 16:45:41 +0700

    fix publish port

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run --publish 8080:8080 -v "$(pwd)/logs:/app/logs" -d --name rete-challenge rete-challenge:latest
+docker run --publish 80:8080 -v "$(pwd)/logs:/app/logs" -d --name rete-challenge rete-challenge:latest
